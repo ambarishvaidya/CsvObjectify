9cd3957daebbe516afb1d65fb2738ab52e26fb90
Ambarish Vaidya <ambarish.vaidya@gmail.com>
2023-05-14 22:55:47 +0100

    Adding sample console project

---
+++ Sample/Program.cs
@@ -0,0 +1,70 @@
+﻿using CsvObjectify;
+using CsvObjectify.Column;
+using CsvObjectify.Column.Helper;
+
+namespace Sample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            new Program();
+        }
+
+        public Program()
+        {
+            ICsvParser<Resident> residentParser = CsvParser<Resident>.Build
+                (
+                    CsvProfile.Build(@"E:\MyData\MyWork\_Github\CsvObjectify\BenchmarkCsvObjectify\CsvFiles\ResidentsWithHeaderDefaultDelimiter.csv",
+                    new CsvObjectify.Column.ColumnMetadata[]
+                    { 
+                        ColumnDefinitionHelper.CreateStringColumn("First Name", "FirstName"),
+                        new ColumnDefinition<string>("Middle Name", s => s.Trim(), "MiddleName"),
+                        new ColumnDefinition<string>(2, s => s.Trim(), "LastName"),
+                        new ColumnDefinition<DateTime>(4, s => DateTime.Parse(s), "DateOfBirth"),
+                        new ColumnDefinition<char>("Disability", s => ParseDisablity(s), "Disability")
+                    }, true)
+                );
+
+            //var obj1 = residentParser.Parse();
+            //var obj2 = residentParser.Parse();
+            //var obj3 = residentParser.Parse();
+            //var obj4 = residentParser.Parse();
+
+            List<Resident> residents = new List<Resident>();
+            foreach (var item in residentParser.ParseParallel())
+                residents.Add(item);
+
+            foreach(var item in residents)
+                Console.WriteLine(item.ToString());
+
+            Console.ReadLine();
+
+        }
+
+        public char ParseDisablity(string b)
+        {
+            return bool.Parse(b) ? 'Y' : 'F';
+        }
+    }
+
+    internal class Resident
+    {
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string Sex { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public bool Married { get; set; }
+        public int Children { get; set; }
+        public string Address { get; set; }
+        public string Employment { get; set; }
+        public char Disability { get; set; }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} {DateOfBirth} - {Disability}";
+        }
+    }    
+}
