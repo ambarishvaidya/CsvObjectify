a922121faab71f04c14c01a45b085900c6667e10
ambarishvaidya <132093368+ambarishvaidya@users.noreply.github.com>
2026-03-15 15:15:36 +0000

    First cut implementation using Span. Benchmark shows 2.2x performance and 87% reduction in memory allocation.

--- CsvObjectify/CsvParser.cs
+++ CsvObjectify/CsvParser.cs
@@ -1,6 +1,7 @@
 ﻿using CsvObjectify.Column;
 using Microsoft.VisualBasic.FileIO;
 using System.Reflection;
+using System.Text;
 
 namespace CsvObjectify
 {
@@ -58,8 +59,106 @@ namespace CsvObjectify
 
             return dictMapping;
         }
-                
-        public IEnumerable<T> Parse()
+
+        public IEnumerable<T> Parse() => ParseWithSpan();
+        
+
+        public IEnumerable<T> ParseWithSpan()
+        {
+            bool ignoreFirstLine = _profile.FileDetails.IsFirstRowHeader;
+            using (StreamReader reader = new StreamReader(_profile.FileDetails.FilePath))
+            {
+                if (ignoreFirstLine)
+                    reader.ReadLine();
+
+                while (!reader.EndOfStream)
+                {
+                    ReadOnlySpan<char> lineData = reader.ReadLine().AsSpan();
+                    if (lineData.Length < 1) continue;
+                    T tObj = new T();
+
+                    try
+                    {
+                        int sliceFrom = 0;
+                        int counter = 0;
+                        int sliceTo = -1;
+                        int indexCounter = -1;
+                        foreach (var kvp in _mappings)
+                        {
+                            sliceTo = kvp.Key;
+                            sliceFrom = kvp.Key - 1;
+
+                            bool quotedData = false;
+
+                            while (indexCounter != sliceFrom)
+                            {
+                                if (lineData[counter] == '"')
+                                {
+                                    quotedData = !quotedData;
+                                }
+                                if (lineData[counter] == _profile.FileDetails.Delimiter && !quotedData)
+                                    indexCounter++;
+                                counter++;
+                            }
+                            sliceFrom = counter - 1;
+
+                            quotedData = false;
+                            while (indexCounter != sliceTo)
+                            {
+                                if (lineData[counter] == '"')
+                                {
+                                    quotedData = !quotedData;
+                                }
+                                if (lineData[counter] == _profile.FileDetails.Delimiter && !quotedData)
+                                    indexCounter++;
+                                counter++;
+                            }
+                            sliceTo = counter - 1;
+
+                            var dataspan = sliceFrom < 0 ? lineData.Slice(0, sliceTo) : lineData.Slice(sliceFrom + 1, sliceTo - sliceFrom - 1);
+
+                            //call the method in mappings to parse the data at kvpindex
+                            object parsedData = kvp.Value.CellDataMethodInfo.Invoke(kvp.Value.ColumnDefnInstance, [Unescape(dataspan)]);
+                            //from the property assign it to tObj
+                            tObj.GetType().InvokeMember(kvp.Value.PropertyName,
+                            BindingFlags.Instance | BindingFlags.Public | BindingFlags.SetProperty,
+                            Type.DefaultBinder, tObj, new object[] { parsedData });
+                        }
+                    }
+                    catch
+                    {
+                        string logMessage = $"Missing or incorrect items for condifured columns in row with data : {lineData.ToString()}";
+                    }
+                    
+                    yield return tObj;
+
+                }
+            }
+        }
+
+        private string Unescape(ReadOnlySpan<char> data)
+        {
+            if (data.Length > 1 && data[0] == '"' && data[^1] == '"')
+                data = data.Slice(1, data.Length - 2);
+
+            StringBuilder stringBuilder = new StringBuilder();
+            for(int counter = 0; counter < data.Length; counter++)
+            {
+                if (data[counter] == '"' && counter + 1 < data.Length && data[counter + 1] == '"')
+                {
+                    stringBuilder.Append('"');
+                    counter++;
+                }
+                else
+                {
+                    stringBuilder.Append(data[counter]);
+                }
+            }
+            return stringBuilder.ToString();
+        }
+
+
+        public IEnumerable<T> ParseWithoutSpan()
         {
             bool ignoreFirstLine = _profile.FileDetails.IsFirstRowHeader;
             using (TextFieldParser reader = new TextFieldParser(_profile.FileDetails.FilePath))
